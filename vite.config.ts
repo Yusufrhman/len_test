fcca99510c21a6fb75056a59154a73d198ceed52
Yusufrhman <zvistyt@gmail.com>
2026-09-19 08:03:39 +0700

    chore: add dependencies, path alias and api environment config

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,18 @@
-import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
+import path from "node:path";
 import tailwindcss from "@tailwindcss/vite";
-
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [tailwindcss(), react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(import.meta.dirname, "./src"),
+    },
+  },
+  server: {
+    port: 5173,
+    host: true,
+  },
 });
